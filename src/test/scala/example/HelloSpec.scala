522905c72770a90066688cba4a41470d6d60e88d
Conny Brunnkvist <conny.brunnkvist@agoda.com>
2020-09-28 17:39:15 +0700

    Some output from testrunner

--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -5,6 +5,8 @@ import org.scalatest.matchers.should.Matchers
 
 class HelloSpec extends AnyFlatSpec with Matchers {
   "The Hello object" should "say hello" in {
+    println("HELLOSPEC STARTED")
     Hello.greeting shouldEqual "hello"
+    println("HELLOSPEC FINISHED")
   }
 }
